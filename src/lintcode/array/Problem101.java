8f5f17b18f59f572ce7e51c0657f6222ee9b91be
Allen <lvshuaino@gmail.com>
2019-09-22 09:49:36 +0800

    101. 删除排序数组中的重复数字 II ：给你一个排序数组，删除其中的重复元素，使得每个数字最多出现两次，返回新的数组的长度.如果一个数字出现超过2次，则这个数字最后保留两个。

---
+++ src/lintcode/array/Problem101.java
@@ -0,0 +1,27 @@
+package lintcode.array;
+
+public class Problem101 {
+    /**
+     * @param : a array of integers
+     * @return : return an integer
+     */
+    public int removeDuplicates(int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int index = 0, count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[index]) {
+                if (count < 2) {
+                    nums[++index] = nums[i];
+                    count++;
+                }
+            } else {
+                nums[++index] = nums[i];
+                count = 1;
+            }
+        }
+        return index + 1;
+    }
+}
