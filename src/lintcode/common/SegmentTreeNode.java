a1c1a13e1685b57bcb812285eb08fd678b397cbc
Allen <lvshuaino@gmail.com>
2019-11-27 23:33:53 +0800

    Problem 105，106

---
+++ src/lintcode/common/SegmentTreeNode.java
@@ -0,0 +1,12 @@
+package lintcode.common;
+
+public class SegmentTreeNode {
+    public int start, end;
+    public SegmentTreeNode left, right;
+
+    public SegmentTreeNode(int start, int end) {
+        this.start = start;
+        this.end = end;
+        this.left = this.right = null;
+    }
+}
